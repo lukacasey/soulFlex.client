6387c052b27861363f8db2df734cb0982345fdda
Luka Casey <lukacasaletto2000@gmail.com>
2023-06-02 12:25:10 +1000

    disable dates after three months

--- src/components/booking/Bookings.js
+++ src/components/booking/Bookings.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-
 // react calendar https://www.npmjs.com/package/react-calendar
 // recipes for more specific details https://github.com/wojtekmaj/react-calendar/wiki/Recipes
 import Calendar from "react-calendar";
-// default React-Calendar styling
 import "react-calendar/dist/Calendar.css";
 
 export default function Bookings() {
@@ -15,10 +13,39 @@ export default function Bookings() {
     console.log(date);
   }
 
+  function tileDisabled({ date, view }) {
+    // Disable past dates
+    if (view === "month" && date <= new Date()) {
+      return true;
+    }
+
+    // disable dates over 3 months
+    const futureLimit = new Date();
+    futureLimit.setMonth(futureLimit.getMonth() + 3); // Set the future limit to three months from today
+    if (date > futureLimit) {
+      return true;
+    }
+
+    // Disable specific dates
+    const disabledDates = [
+      // Add your disabled dates here
+      new Date("2023-06-06"),
+      new Date("2023-06-07"),
+    ];
+
+    return disabledDates.some(
+      (disabledDate) => date.toDateString() === disabledDate.toDateString()
+    );
+  }
+
   return (
     <>
       <div>
-        <Calendar onChange={onChange} value={value} />
+        <Calendar
+          onChange={onChange}
+          value={value}
+          tileDisabled={tileDisabled}
+        />
       </div>
     </>
   );
