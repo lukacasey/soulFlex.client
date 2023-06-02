cb427c9830d9f6705aee1d8b058a305e9f8dbba1
Luka Casey <lukacasaletto2000@gmail.com>
2023-06-02 19:09:14 +1000

    fixing calendar stuff

--- src/components/booking/Bookings.js
+++ src/components/booking/Bookings.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-// react calendar https://www.npmjs.com/package/react-calendar
-// recipes for more specific details https://github.com/wojtekmaj/react-calendar/wiki/Recipes
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
@@ -10,29 +8,31 @@ export default function Bookings() {
 
   function onChange(date) {
     setValue(date);
-    console.log(date);
+    console.log(date.toDateString());
   }
 
   function tileDisabled({ date, view }) {
-    // Disable past dates
+    // disable past dates
     if (view === "month" && date <= new Date()) {
       return true;
     }
 
-    // disable dates over 3 months
+    // disable dates 3 months or more from current
     const futureLimit = new Date();
     futureLimit.setMonth(futureLimit.getMonth() + 3); // Set the future limit to three months from today
     if (date > futureLimit) {
       return true;
     }
 
-    // Disable specific dates
-    const disabledDates = [
-      // Add your disabled dates here
-      new Date("2023-06-06"),
-      new Date("2023-06-07"),
-    ];
+    // disable sundays
+    if (date.getDay() === 0) {
+      return true;
+    }
 
+    // dates to be disabled
+    const disabledDates = [new Date("2023-06-13"), new Date("2023-06-14")];
+
+    // convert to string and disable
     return disabledDates.some(
       (disabledDate) => date.toDateString() === disabledDate.toDateString()
     );
